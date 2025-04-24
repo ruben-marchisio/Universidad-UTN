c6fedf753a1cb2356ca61dc9d8965dbc4c9da69a
ruben marchisio <rubenmarchisio@gmial.com>
2025-04-23 23:55:18 -0300

    se trabajo con la clase 2 de python

---
+++ python/clase2/variables1.py
@@ -0,0 +1,23 @@
+#se crean las variables 
+numero = 5
+print (numero)
+texto = "ruben"
+print(texto)
+numeroDecimal = 1.70
+print(numeroDecimal)
+
+#se pueden sumar 
+x = 10
+y = 5
+suma = x + y
+print (suma)
+
+#reutilizamos y se pueden concatenar 
+x = "ruben "
+y = "marchisio"
+suma = x + y
+print(suma)
+
+#podemos ver la direccion de memoria ram 
+
+print("la direccin es",id(x))

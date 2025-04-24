23a8260b5cc57c72a4122a684e6f4d7e0fd4d9a8
ruben marchisio <rubenmarchisio@gmial.com>
2025-04-24 11:30:42 -0300

    realiso las clases de js la 2 y 3

---
+++ javaScript/clase2/holaMundo.js
@@ -0,0 +1,6 @@
+//creamos el hola mundo desde js
+console.log("Hola Mundo desde JS");
+
+//podemos almacenar el resultado en una variable
+let resultado = "Hola Mundo desde JS en una variable";
+console.log(resultado);
---
+++ javaScript/clase3/tiposDeDatos.js
@@ -0,0 +1,20 @@
+// Tipos de datos en JavaScript
+// En JavaScript, los tipos de datos son dinámicos y se pueden cambiar en tiempo de ejecución.
+let nombre = "Ruben"; //string
+let edad = 30; //number 
+let esEstudiante = true; //boolean
+let altura = 1.75; //number
+console.log(`Hola, mi nombre es ${nombre}, tengo ${edad} años y mido ${altura} metros.`); //string
+
+// esto es un objeto
+// Un objeto es una colección de propiedades y métodos.
+let Object = { 
+    nombre: "Ruben",
+    edad: 30,
+    esEstudiante: true,
+    altura: 1.75
+}; 
+
+console.log(Object);
+//lo podemos ejecutar tambien en el archivo html
+// <script src="tiposDeDatos.js"></script>

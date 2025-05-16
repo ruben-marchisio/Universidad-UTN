f0f8841685f61eb79f31634e741271e1d70cc220
ruben marchisio <rubenmarchisio@gmial.com>
2025-05-16 18:39:35 -0300

    Se realizo la actividad 5 de java

---
+++ java/HolaMundo/src/clase5/PrimitivoChar.java
@@ -0,0 +1,28 @@
+public class PrimitivoChar {
+    public static void main(String[] args) {
+        //tipos de primitivos char
+        char miVariableChar = 'a';
+        System.out.println(miVariableChar);
+        char varCaracter = '\u0024';
+        System.out.println(varCaracter);
+        char varCaracterDecimal = 36; // valor decimal del juego de caracteres unicode
+        System.out.println("varCaracterDecimal" + varCaracterDecimal);
+        char varCaracterSimbolo = '$';
+        System.out.println("varCaracterSimbolo" + varCaracterSimbolo);
+        
+        var miVariableChar1 = 'a';
+        System.out.println(miVariableChar1);
+        var varCaracter1 = '\u0024';
+        System.out.println(varCaracter1);
+        var varCaracterDecimal1 = 36; // valor decimal del juego de caracteres unicode
+        System.out.println("varCaracterDecimal1" + varCaracterDecimal1);
+        var varCaracterSimbolo1 = '$';
+        System.out.println("varCaracterSimbolo1" + varCaracterSimbolo1);
+        
+        int varEnteroChar = '$';// lo hace un valor decimal 36
+        System.out.println("varEnteroChar" + varEnteroChar);
+        int caracterChar = 'b'; //mostrara el numero que le corresponde 
+        System.out.println("caracterChar" + caracterChar);
+        
+    }
+}
---
+++ java/HolaMundo/src/clase5/primitivos.java
@@ -0,0 +1,13 @@
+
+public class primitivos {
+    public static void main(String[] args) {
+        var numeroEntero = 20; // las literales sin punto automaticamente son de tipo int 
+        System.out.println(numeroEntero);
+        var numFloat = 12.2F; // automaticamente con el punto se trasforma en tipo double 
+        // colocanto F ya sabe que es un float
+        System.out.println("numFloat" + numFloat);
+        var numDouble = 10.0;
+        System.out.println("numDouble" + numDouble);
+              
+    }
+}

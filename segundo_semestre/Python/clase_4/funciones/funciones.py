154125b118b5216c92184366262eda8c6924ad84
Ruben M <rubenmarchisio@gmail.com>
2025-09-01 21:42:29 -0300

    se agrego funciones a la clase 4 de py

---
+++ segundo_semestre/Python/clase_4/funciones/funciones.py
@@ -0,0 +1,10 @@
+# 5.4 Funciones en Python
+# Una función es un bloque de código que solo se ejecuta cuando es llamado.
+# Las funciones pueden recibir datos, llamados parámetros, y pueden devolver datos como resultado.
+# En Python, las funciones se definen usando la palabra clave `def`, seguida del nombre
+
+def mi_funcion():
+    print("Hola desde mi función!")
+
+# llamamos a la función
+mi_funcion()
